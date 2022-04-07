14447d647b0728c2eb094b2009af68c504ba4509
Patrick Rallings <patrickrallings@Patricks-MBP.localdomain>
2022-04-07 17:43:54 -0400

    Started on script

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const cityVal = document.querySelector('input').value
+const tempVal = document.querySelector('#tempActual').innerHTML
+const tempFrom = 'convert from Kal here'
+const tempTo;
+
+const farToCel = () => {
+  return tempTo (tempFrom - 32) x .5556
+}
 
 console.log(cityVal)
