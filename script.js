9a4a48d19a56ed4ca113040f59e406688bac3cd5
Patrick Rallings <patrickrallings@Patricks-iMac.local>
2022-04-10 17:35:16 -0400

    Changed fetch function to include params for units and degrees (to switch between f and c degrees easily)

--- script.js
+++ script.js
@@ -8,46 +8,55 @@ const degreeBtn = document.querySelector("#degreeBtn");
 const currentTemp = document.querySelector("#currentTemp");
 const currentHigh = document.querySelector("#currentHigh");
 const currentLow = document.querySelector("#currentLow");
-const currentConditionText = document.querySelector('#currentConditionText')
+const currentConditionText = document.querySelector("#currentConditionText");
 const currentHumidity = document.querySelector("#humidity");
 const currentFeels = document.querySelector("#feelsLike");
 
 const day1Date = document.querySelector("#day1Date");
 const day1Range = document.querySelector("#day1Range");
 const day1Condition = document.querySelector("#day1Condition");
-const day1ConditionText = document.querySelector('#day1ConditionText');
+const day1ConditionText = document.querySelector("#day1ConditionText");
 
 const day2Date = document.querySelector("#day2Date");
 const day2Range = document.querySelector("#day2Range");
 const day2Condition = document.querySelector("#day2Condition");
-const day2ConditionText = document.querySelector('#day2ConditionText');
+const day2ConditionText = document.querySelector("#day2ConditionText");
 
 const day3Date = document.querySelector("#day3Date");
 const day3Range = document.querySelector("#day3Range");
 const day3Condition = document.querySelector("#day3Condition");
-const day3ConditionText = document.querySelector('#day3ConditionText');
+const day3ConditionText = document.querySelector("#day3ConditionText");
 
 let crd = document.querySelectorAll(".crd");
 let playing = false;
 
-const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const week = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 let today = new Date();
-let currentDay = week[today.getDay()]
+let currentDay = week[today.getDay()];
 let dd = String(today.getDate()).padStart(2, "0");
 let mm = String(today.getMonth() + 1).padStart(2, "0");
 let yyyy = today.getFullYear();
 
-let day1Day = week[today.getDay()+1]
-let day2Day = week[today.getDay()+2]
-let day3Day = week[today.getDay()+3]
+let day1Day = week[today.getDay() + 1];
+let day2Day = week[today.getDay() + 2];
+let day3Day = week[today.getDay() + 3];
 
 today = mm + "/" + dd + "/" + yyyy;
 
 let linkVal;
 
 const backgroundObj = {
-  "01d": "https://img.wattpad.com/dd954c704a777dc1a1f2a948085b7fd888cdd621/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f4d624d55756f67683832624236673d3d2d3739393338303530302e313564316461663930616162633064393139393835343639333537372e676966",
+  "01d":
+    "https://img.wattpad.com/dd954c704a777dc1a1f2a948085b7fd888cdd621/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f4d624d55756f67683832624236673d3d2d3739393338303530302e313564316461663930616162633064393139393835343639333537372e676966",
   "01n": "https://thumbs.gfycat.com/ContentJitteryCats-size_restricted.gif",
   "02d":
     "https://bestanimations.com/media/sky/1132213612blue-sky-clouds-gif.gif",
@@ -80,36 +89,35 @@ const backgroundObj = {
     "http://68.media.tumblr.com/55ab69e8654f7b40c0f6855c8ca22961/tumblr_omjk2lVUt31tliyzbo1_540.gif",
 };
 
-const currentFetchF = () => {
+const currentFetch = (units, degrees) => {
   let cityVal = document.querySelector("input").value;
   date.innerHTML = `<em>${currentDay}, ${today}</em>`;
-  if (cityVal === '') {
-    linkVal = `q=Charlotte,us`
-  }
-  else if (parseInt(cityVal) === Number) {
+  if (cityVal === "") {
+    linkVal = `q=Charlotte,us`;
+  } else if (parseInt(cityVal) === Number) {
     linkVal = `zip=${cityVal},us`;
   } else {
     linkVal = `q=${cityVal},us`;
   }
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?${linkVal}&units=imperial&appid=c459dafd68fb96e886da90dd07f0e224`
+    `https://api.openweathermap.org/data/2.5/weather?${linkVal}&units=${units}&appid=c459dafd68fb96e886da90dd07f0e224`
   )
     .then((response) => response.json())
     .then((json) => {
-      console.log(json)
+      console.log(json);
       let icon = json.weather[0].icon;
       loc.innerHTML = json.name;
       let backgroundImg = backgroundObj[icon];
       console.log(backgroundImg);
       appBottom.style.backgroundImage = `url('${backgroundImg}')`;
       currentTemp.innerHTML =
-        Math.round(json.main.temp) + "<sup style='font-size: 20px;'>℉</sup>";
+        Math.round(json.main.temp) + `<sup style='font-size: 20px;'>${degrees}</sup>`;
       currentHigh.innerHTML =
         Math.round(json.main.temp_max) +
-        "<sup style='font-size: 15px;'>℉</sup>";
+        `<sup style='font-size: 15px;'>${degrees}</sup>`;
       currentLow.innerHTML =
         Math.round(json.main.temp_min) +
-        "<sup style='font-size: 15px;'>℉</sup>";
+        `<sup style='font-size: 15px;'>${degrees}</sup>`;
       document.querySelector(
         "#condition"
       ).src = `https://openweathermap.org/img/wn/${icon}.png`;
@@ -118,28 +126,40 @@ const currentFetchF = () => {
       currentHumidity.innerHTML = json.main.humidity + "%";
       currentFeels.innerHTML =
         Math.round(json.main.feels_like) +
-        "<sup style='font-size: 15px;'>℉</sup>";
-      let lat = json.coord.lat
-      let lon = json.coord.lon
+        `<sup style='font-size: 15px;'>${degrees}</sup>`;
+      let lat = json.coord.lat;
+      let lon = json.coord.lon;
       fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=dba94717f4d86654f333b634ebb619f4`
-        )
-      .then((response) => response.json())
-      .then((futJson) => {
-      console.log(futJson);
-      day1Date.innerHTML = day1Day
-      day2Date.innerHTML = day2Day
-      day3Date.innerHTML = day3Day
-      day1Range.innerHTML = `${Math.round(futJson.daily[1].temp.max)}<sup style='font-size: 10px;'>℉</sup> / ${Math.round(futJson.daily[1].temp.min)}<sup style='font-size: 10px;'>℉</sup>`
-      day2Range.innerHTML = `${Math.round(futJson.daily[2].temp.max)}<sup style='font-size: 10px;'>℉</sup> / ${Math.round(futJson.daily[2].temp.min)}<sup style='font-size: 10px;'>℉</sup>`
-      day3Range.innerHTML = `${Math.round(futJson.daily[3].temp.max)}<sup style='font-size: 10px;'>℉</sup> / ${Math.round(futJson.daily[3].temp.min)}<sup style='font-size: 10px;'>℉</sup>`
-      day1Condition.src = `https://openweathermap.org/img/wn/${futJson.daily[1].weather[0].icon}.png`
-      day1ConditionText.innerHTML = `<em>${futJson.daily[1].weather[0].description}</em>`
-      day2Condition.src = `https://openweathermap.org/img/wn/${futJson.daily[2].weather[0].icon}.png`
-      day2ConditionText.innerHTML = `<em>${futJson.daily[2].weather[0].description}</em>`
-      day3Condition.src = `https://openweathermap.org/img/wn/${futJson.daily[3].weather[0].icon}.png`
-      day3ConditionText.innerHTML = `<em>${futJson.daily[3].weather[0].description}</em>`
-      });
+        `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly,alerts&units=imperial&appid=dba94717f4d86654f333b634ebb619f4`
+      )
+        .then((response) => response.json())
+        .then((futJson) => {
+          console.log(futJson);
+          day1Date.innerHTML = day1Day;
+          day2Date.innerHTML = day2Day;
+          day3Date.innerHTML = day3Day;
+          day1Range.innerHTML = `${Math.round(
+            futJson.daily[1].temp.max
+          )}<sup style='font-size: 10px;'>${degrees}</sup> / ${Math.round(
+            futJson.daily[1].temp.min
+          )}<sup style='font-size: 10px;'>${degrees}</sup>`;
+          day2Range.innerHTML = `${Math.round(
+            futJson.daily[2].temp.max
+          )}<sup style='font-size: 10px;'>${degrees}</sup> / ${Math.round(
+            futJson.daily[2].temp.min
+          )}<sup style='font-size: 10px;'>${degrees}</sup>`;
+          day3Range.innerHTML = `${Math.round(
+            futJson.daily[3].temp.max
+          )}<sup style='font-size: 10px;'>${degrees}</sup> / ${Math.round(
+            futJson.daily[3].temp.min
+          )}<sup style='font-size: 10px;'>${degrees}</sup>`;
+          day1Condition.src = `https://openweathermap.org/img/wn/${futJson.daily[1].weather[0].icon}.png`;
+          day1ConditionText.innerHTML = `<em>${futJson.daily[1].weather[0].description}</em>`;
+          day2Condition.src = `https://openweathermap.org/img/wn/${futJson.daily[2].weather[0].icon}.png`;
+          day2ConditionText.innerHTML = `<em>${futJson.daily[2].weather[0].description}</em>`;
+          day3Condition.src = `https://openweathermap.org/img/wn/${futJson.daily[3].weather[0].icon}.png`;
+          day3ConditionText.innerHTML = `<em>${futJson.daily[3].weather[0].description}</em>`;
+        });
     });
 };
 
@@ -159,16 +179,16 @@ const flip = (target) => {
   });
 };
 
-currentFetchF();
+currentFetch('imperial', '℉');
 
 searchBtn.addEventListener("click", function () {
   flip(crd);
-  setTimeout(currentFetchF, 500);
+  setTimeout(currentFetch, 500);
 });
 
-document.querySelector('input').addEventListener('keyup', function(event) {
-  if (event.key === 'Enter') {
-    console.log('keyup test')
-    document.getElementById('searchBtn').click()
+document.querySelector("input").addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    console.log("keyup test");
+    document.getElementById("searchBtn").click();
   }
-})
+});
